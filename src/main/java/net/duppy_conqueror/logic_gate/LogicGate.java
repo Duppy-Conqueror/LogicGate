5435554e6920f3d3d944a67e7f16fe2a1ea31b6f
Duppy_Conqueror <duppyconqueror206@gmail.com>
2025-07-25 18:25:22 +0800

    Initial commit

---
+++ src/main/java/net/duppy_conqueror/logic_gate/LogicGate.java
@@ -0,0 +1,14 @@
+package net.duppy_conqueror.logic_gate;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogicGate implements ModInitializer {
+	public static final String MOD_ID = "logic_gate";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {}
+}
---
+++ src/main/java/net/duppy_conqueror/logic_gate/LogicGateClient.java
@@ -0,0 +1,8 @@
+package net.duppy_conqueror.logic_gate;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LogicGateClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {}
+}
