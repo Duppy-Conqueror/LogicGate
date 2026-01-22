51545d862b5958e3da5958cf5088cf909b650223
Duppy_Conqueror <duppyconqueror206@gmail.com>
2026-01-22 17:53:54 +0800

    feat: Remove unused client entrypoint

--- src/main/java/net/duppy_conqueror/logic_gate/LogicGateClient.java
+++
@@ -1,8 +0,0 @@
-package net.duppy_conqueror.logic_gate;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class LogicGateClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {}
-}
