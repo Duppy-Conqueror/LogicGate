a0d8c8b0b83208ac6d906b22416b57ca43615953
Duppy_Conqueror <duppyconqueror206@gmail.com>
2026-01-22 18:07:41 +0800

    feat: Enum representing logic mode

---
+++ src/main/java/net/duppy_conqueror/logic_gate/block/enums/LogicGateMode.java
@@ -0,0 +1,62 @@
+package net.duppy_conqueror.logic_gate.block.enums;
+
+import net.minecraft.util.StringIdentifiable;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public enum LogicGateMode implements StringIdentifiable {
+    BUFFER("buffer"),
+    NOT("not"),
+    OR("or"),
+    AND("and"),
+    XOR("xor"),
+    NOR("nor"),
+    NAND("nand"),
+    IMPLY("imply"),
+    NIMPLY("nimply");
+
+    private static Iterator<LogicGateMode> getIterator(boolean reverse) {
+        if (reverse) {
+            List<LogicGateMode> enumValues = Arrays.asList(values());
+            Collections.reverse(enumValues);
+            return enumValues.iterator();
+        } else {
+            return Arrays.asList(values()).iterator();
+        }
+    }
+
+    public static LogicGateMode cycle(LogicGateMode currentMode, boolean reverse) {
+        Iterator<LogicGateMode> iterator = getIterator(reverse);
+        while (iterator.hasNext()) {
+            if (iterator.next().equals(currentMode)) {
+                if (iterator.hasNext()) {
+                    return iterator.next();
+                }
+                return getIterator(reverse).next();
+            }
+        }
+        return iterator.next();
+    }
+
+    private final String name;
+
+    private LogicGateMode(String name) {
+        this.name = name;
+    }
+
+    public boolean isSingleInput() {
+        return this == BUFFER || this == NOT;
+    }
+
+    public String toString() {
+        return this.name;
+    }
+
+    @Override
+    public String asString() {
+        return this.name;
+    }
+}
