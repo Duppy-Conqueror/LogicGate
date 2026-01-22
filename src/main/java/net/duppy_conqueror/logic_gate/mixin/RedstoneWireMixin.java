6c931ab2791d1876b3d7edc87fd5f8a9b07c7a72
Duppy_Conqueror <duppyconqueror206@gmail.com>
2026-01-22 18:25:14 +0800

    feat: Add mechanics between logic gate and redstone wires

---
+++ src/main/java/net/duppy_conqueror/logic_gate/mixin/RedstoneWireMixin.java
@@ -0,0 +1,29 @@
+package net.duppy_conqueror.logic_gate.mixin;
+
+import net.duppy_conqueror.logic_gate.block.LogicGateBlock;
+import net.duppy_conqueror.logic_gate.block.ModBlocks;
+import net.duppy_conqueror.logic_gate.block.enums.LogicGateMode;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.RedstoneWireBlock;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.Direction;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(RedstoneWireBlock.class)
+public class RedstoneWireMixin {
+    @Inject(at = @At("HEAD"), method = "connectsTo(Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/Direction;)Z", cancellable = true)
+    private static void disconnectFromLogicGate(BlockState state, Direction dir, CallbackInfoReturnable<Boolean> cir) {
+        if (state.isOf(ModBlocks.LOGIC_GATE) && dir != null) {
+            LogicGateMode mode = state.get(LogicGateBlock.MODE);
+            Direction facing = state.get(Properties.HORIZONTAL_FACING);
+            if (mode.isSingleInput()) {
+                cir.setReturnValue(facing.getAxis() == dir.getAxis());
+            } else {
+                cir.setReturnValue(!facing.equals(dir.getOpposite()));
+            }
+        }
+    }
+}
