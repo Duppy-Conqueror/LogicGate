00c27ceade9a45622854cabfc554f490636acc09
Duppy_Conqueror <duppyconqueror206@gmail.com>
2026-01-22 17:59:55 +0800

    feat: Mod main entrypoint

--- src/main/java/net/duppy_conqueror/logic_gate/LogicGateMod.java
+++ src/main/java/net/duppy_conqueror/logic_gate/LogicGateMod.java
@@ -5,10 +5,12 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LogicGate implements ModInitializer {
+public class LogicGateMod implements ModInitializer {
 	public static final String MOD_ID = "logic_gate";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
-	public void onInitialize() {}
+	public void onInitialize() {
+		LOGGER.info("Initializing mod: " + MOD_ID);
+	}
 }
