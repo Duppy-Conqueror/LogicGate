0a49112b39b5454042e721ec781a576ef18c2f34
Duppy_Conqueror <duppyconqueror206@gmail.com>
2026-01-22 19:41:39 +0800

    refactor: Move loot table to data generation

--- src/main/java/net/duppy_conqueror/logic_gate/LogicGateModDataGenerator.java
+++ src/main/java/net/duppy_conqueror/logic_gate/LogicGateModDataGenerator.java
@@ -1,5 +1,6 @@
 package net.duppy_conqueror.logic_gate;
 
+import net.duppy_conqueror.logic_gate.datagen.ModBlockLootTableProvider;
 import net.duppy_conqueror.logic_gate.datagen.ModModelProvider;
 import net.duppy_conqueror.logic_gate.datagen.ModRecipeProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
@@ -11,5 +12,6 @@ public class LogicGateModDataGenerator implements DataGeneratorEntrypoint {
 		FabricDataGenerator.Pack pack = dataGenerator.createPack();
 		pack.addProvider(ModRecipeProvider::new);
 		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModBlockLootTableProvider::new);
 	}
 }
---
+++ src/main/java/net/duppy_conqueror/logic_gate/datagen/ModBlockLootTableProvider.java
@@ -0,0 +1,16 @@
+package net.duppy_conqueror.logic_gate.datagen;
+
+import net.duppy_conqueror.logic_gate.block.ModBlocks;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+
+public class ModBlockLootTableProvider extends FabricBlockLootTableProvider {
+    public ModBlockLootTableProvider(FabricDataOutput dataOutput) {
+        super(dataOutput);
+    }
+
+    @Override
+    public void generate() {
+        addDrop(ModBlocks.LOGIC_GATE);
+    }
+}
