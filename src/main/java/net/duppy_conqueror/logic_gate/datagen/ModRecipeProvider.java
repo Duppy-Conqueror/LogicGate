706ce0edd25dd381182ed47a24783855b3c155b0
Duppy_Conqueror <duppyconqueror206@gmail.com>
2026-01-31 17:09:59 +0800

    refactor: Migrate from Consumer<RecipeJsonProvider> to RecipeExporter

--- src/main/java/net/duppy_conqueror/logic_gate/datagen/ModRecipeProvider.java
+++ src/main/java/net/duppy_conqueror/logic_gate/datagen/ModRecipeProvider.java
@@ -4,7 +4,7 @@ import net.duppy_conqueror.logic_gate.LogicGateMod;
 import net.duppy_conqueror.logic_gate.block.ModBlocks;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
@@ -18,7 +18,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
     }
 
     @Override
-    public void generate(Consumer<RecipeJsonProvider> exporter) {
+    public void generate(RecipeExporter exporter) {
         ShapedRecipeJsonBuilder.create(RecipeCategory.REDSTONE, ModBlocks.LOGIC_GATE)
                 .pattern("I#I")
                 .pattern("#X#")
