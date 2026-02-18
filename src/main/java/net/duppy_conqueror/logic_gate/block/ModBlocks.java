c8303836f8e2afd355bf611c60e75f3a7bedb55e
Duppy_Conqueror <duppyconqueror206@gmail.com>
2026-02-19 01:57:20 +0800

    refactor: Follow changes on registeries

--- src/main/java/net/duppy_conqueror/logic_gate/block/ModBlocks.java
+++ src/main/java/net/duppy_conqueror/logic_gate/block/ModBlocks.java
@@ -8,22 +8,36 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
+import java.util.function.Function;
+
 public class ModBlocks {
-    public static final Block LOGIC_GATE = registerBlock("logic_gate", new LogicGateBlock(AbstractBlock.Settings.create().breakInstantly().sounds(BlockSoundGroup.WOOD).pistonBehavior(PistonBehavior.DESTROY)));
+    public static final Block LOGIC_GATE;
 
-    private static Block registerBlock(String name, Block block) {
-        registerBlockItem(name, block);
-        return Registry.register(Registries.BLOCK, Identifier.of(LogicGateMod.MOD_ID, name), block);
-    }
-
-    private static Item registerBlockItem(String name, Block block) {
-        return Registry.register(Registries.ITEM, Identifier.of(LogicGateMod.MOD_ID, name), new BlockItem(block, new Item.Settings()));
+    static {
+        LOGIC_GATE = registerBlock("logic_gate",
+            LogicGateBlock::new,
+            AbstractBlock.Settings.create().breakInstantly().sounds(BlockSoundGroup.WOOD).pistonBehavior(PistonBehavior.DESTROY),
+            true
+        );
     }
 
     public static void registerBlocks() {
         LogicGateMod.LOGGER.info("Registering blocks for " + LogicGateMod.MOD_ID);
+    }
+
+    private static Block registerBlock(String name, Function<AbstractBlock.Settings, Block> blockFactory, AbstractBlock.Settings settings, boolean registerItem) {
+        Identifier blockId = Identifier.of(LogicGateMod.MOD_ID, name);
+        RegistryKey<Block> blockRegistryKey = RegistryKey.of(RegistryKeys.BLOCK, blockId);
+        Block block = blockFactory.apply(settings.registryKey(blockRegistryKey));
+        if (registerItem) {
+            RegistryKey<Item> itemRegistryKey = RegistryKey.of(RegistryKeys.ITEM, blockId);
+            Registry.register(Registries.ITEM, blockId, new BlockItem(block, new Item.Settings().useBlockPrefixedTranslationKey().registryKey(itemRegistryKey)));
+        }
+        return Registry.register(Registries.BLOCK, blockId, block);
     }
 }
