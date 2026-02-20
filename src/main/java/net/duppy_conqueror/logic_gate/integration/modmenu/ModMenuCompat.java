a2c5771d15e6dc65bbf7ecc6ba8dcfa078461036
Duppy_Conqueror <duppyconqueror206@gmail.com>
2026-02-20 23:45:29 +0800

    deps: Call static method from AutoConfigClient instead of AutoConfig

--- src/main/java/net/duppy_conqueror/logic_gate/integration/modmenu/ModMenuCompat.java
+++ src/main/java/net/duppy_conqueror/logic_gate/integration/modmenu/ModMenuCompat.java
@@ -2,7 +2,7 @@ package net.duppy_conqueror.logic_gate.integration.modmenu;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfigClient;
 import net.duppy_conqueror.logic_gate.config.ModConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -12,7 +12,7 @@ import net.minecraft.client.gui.screen.Screen;
 public class ModMenuCompat implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return (Screen parent) -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
+        return (Screen parent) -> AutoConfigClient.getConfigScreen(ModConfig.class, parent).get();
     }
 }
 
