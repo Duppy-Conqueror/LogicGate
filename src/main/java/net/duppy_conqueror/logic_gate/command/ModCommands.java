62340f3ca754eb2bee34e223ec1f1e00dcabb9ff
Duppy_Conqueror <duppyconqueror206@gmail.com>
2026-03-01 02:11:28 +0800

    refactor: Rewrite logic gate delay command

--- src/main/java/net/duppy_conqueror/logic_gate/command/ModCommands.java
+++ src/main/java/net/duppy_conqueror/logic_gate/command/ModCommands.java
@@ -5,8 +5,8 @@ import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
 public class ModCommands {
     public static void init() {
-        CommandRegistrationCallback.EVENT.register(SetGateDelayCommand::register);
         LogicGateMod.LOGGER.info("Registering commands for " + LogicGateMod.MOD_ID);
+        CommandRegistrationCallback.EVENT.register(GateDelayCommand::register);
         CommandRegistrationCallback.EVENT.register(SmartRedstoneWireConnectionCommand::register);
     }
 }
