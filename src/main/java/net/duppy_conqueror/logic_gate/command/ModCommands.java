62b7b6ca4a0710466b8154f0ffd528fc0d7addcd
Duppy_Conqueror <duppyconqueror206@gmail.com>
2026-03-01 02:10:45 +0800

    feat: Builtin Minecraft command to toggle smart redstone wire connection

--- src/main/java/net/duppy_conqueror/logic_gate/command/ModCommands.java
+++ src/main/java/net/duppy_conqueror/logic_gate/command/ModCommands.java
@@ -1,9 +1,12 @@
 package net.duppy_conqueror.logic_gate.command;
 
+import net.duppy_conqueror.logic_gate.LogicGateMod;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
 public class ModCommands {
     public static void init() {
         CommandRegistrationCallback.EVENT.register(SetGateDelayCommand::register);
+        LogicGateMod.LOGGER.info("Registering commands for " + LogicGateMod.MOD_ID);
+        CommandRegistrationCallback.EVENT.register(SmartRedstoneWireConnectionCommand::register);
     }
 }
