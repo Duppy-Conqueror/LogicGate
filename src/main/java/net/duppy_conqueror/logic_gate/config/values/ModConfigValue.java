0652ffa00b0f81c36fcf4222ba64531bb638849d
Duppy_Conqueror <duppyconqueror206@gmail.com>
2026-03-01 02:00:34 +0800

    feat: Definitions for config value entry

---
+++ src/main/java/net/duppy_conqueror/logic_gate/config/values/ModConfigValue.java
@@ -0,0 +1,52 @@
+package net.duppy_conqueror.logic_gate.config.values;
+
+import net.duppy_conqueror.logic_gate.config.ModConfigEntry;
+import org.jetbrains.annotations.Nullable;
+import net.minecraft.text.Text;
+
+import java.util.function.Supplier;
+
+public abstract class ModConfigValue<T> extends ModConfigEntry implements Supplier<T> {
+    protected final T defaultValue;
+    protected T value;
+    private String translationKey;
+    private String descriptionKey;
+
+    protected ModConfigValue(String id, T defaultValue) {
+        super(id);
+        this.defaultValue = defaultValue;
+    }
+
+    public T getDefaultValue() {
+        return this.defaultValue;
+    }
+
+    public abstract boolean isValid(T value);
+
+    public void set(T newValue) {
+        this.value = newValue;
+    }
+
+    @Override
+    public T get() {
+        return this.value;
+    }
+
+    public void setTranslationKey(String translationKey) {
+        this.translationKey = translationKey;
+    }
+
+    public void setDescriptionKey(String descriptionKey) {
+        this.descriptionKey = descriptionKey;
+    }
+
+    public Text getTranslation() {
+        return Text.translatable(translationKey);
+    }
+
+    @Nullable
+    public Text getDescription() {
+        return this.descriptionKey == null ? null : Text.translatable(this.descriptionKey);
+    }
+
+}
