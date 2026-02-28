2e3a441290db1ddd97e54d3ea8152ea77c178451
Duppy_Conqueror <duppyconqueror206@gmail.com>
2026-03-01 02:06:29 +0800

    feat: Compat class for YACL

---
+++ src/main/java/net/duppy_conqueror/logic_gate/integration/yacl/YACLCompat.java
@@ -0,0 +1,87 @@
+package net.duppy_conqueror.logic_gate.integration.yacl;
+
+import dev.isxander.yacl3.api.*;
+import dev.isxander.yacl3.api.controller.IntegerSliderControllerBuilder;
+import dev.isxander.yacl3.api.controller.TickBoxControllerBuilder;
+
+import net.duppy_conqueror.logic_gate.config.ModConfigEntry;
+import net.duppy_conqueror.logic_gate.config.ModConfigHolder;
+import net.duppy_conqueror.logic_gate.config.ModConfigSubCategory;
+import net.duppy_conqueror.logic_gate.config.values.BooleanModConfigValue;
+import net.duppy_conqueror.logic_gate.config.values.IntegerModConfigValue;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+
+public class YACLCompat {
+    public static Screen makeScreen(Screen parent, ModConfigHolder modConfigHolder) {
+        modConfigHolder.loadConfig();
+
+        YetAnotherConfigLib.Builder builder = YetAnotherConfigLib.createBuilder();
+        builder.title(modConfigHolder.getMainEntry().getTranslation());
+        builder.save(modConfigHolder::saveConfig);
+
+        for (ModConfigEntry configEntry: modConfigHolder.getMainEntry().getEntries()) {
+            if (!(configEntry instanceof ModConfigSubCategory subCategory)) {
+                continue;
+            }
+
+            ConfigCategory.Builder mainCategory = ConfigCategory.createBuilder().name(subCategory.getTranslation());
+
+            for (ModConfigEntry innerConfigEntry: subCategory.getEntries()) {
+                if (innerConfigEntry instanceof ModConfigSubCategory innerSubCategory) {
+                    OptionGroup.Builder subCategoryBuilder = OptionGroup.createBuilder().name(innerSubCategory.getTranslation()).collapsed(true);
+                    YACLCompat.addEntriesRecursive(mainCategory, subCategoryBuilder, innerSubCategory);
+                    mainCategory.group(subCategoryBuilder.build());
+                } else {
+                    mainCategory.option(YACLCompat.buildEntry(innerConfigEntry));
+                }
+            }
+
+            builder.category(mainCategory.build());
+        }
+
+        return builder.build().generateScreen(parent);
+    }
+
+    private static void addEntriesRecursive(ConfigCategory.Builder builder, OptionGroup.Builder subCategoryBuilder, ModConfigSubCategory subCategory) {
+        for (ModConfigEntry configEntry: subCategory.getEntries()) {
+            if (configEntry instanceof ModConfigSubCategory subSubCategory) {
+//                OptionGroup.Builder subSubCategoryBuilder = OptionGroup.createBuilder().name(subSubCategory.getTranslation()).collapsed(true);
+//                YACLCompat.addEntriesRecursive(builder, subSubCategoryBuilder, subSubCategory);
+//                subCategoryBuilder.group(subSubCategoryBuilder.build());
+            } else {
+                subCategoryBuilder.option(YACLCompat.buildEntry(configEntry));
+            }
+        }
+    }
+
+    private static Option<?> buildEntry(ModConfigEntry configEntry) {
+
+        if (configEntry instanceof IntegerModConfigValue icv) {
+            Option.Builder<Integer> intOptionBuilder = Option.<Integer>createBuilder()
+                .name(icv.getTranslation())
+                .binding(icv.getDefaultValue(), icv, icv::set)
+                .controller(oi -> IntegerSliderControllerBuilder.create(oi)
+                    .range(icv.getMin(), icv.getMax())
+                    .step(1)
+                );
+            Text descriptionText = icv.getDescription();
+            if (descriptionText != null) {
+                intOptionBuilder.description(OptionDescription.of(descriptionText));
+            }
+            return intOptionBuilder.build();
+        } else if (configEntry instanceof BooleanModConfigValue bcv) {
+            Option.Builder<Boolean> boolOptionBuilder = Option.<Boolean>createBuilder()
+                .name(bcv.getTranslation())
+                .binding(bcv.getDefaultValue(), bcv, bcv::set)
+                .controller(TickBoxControllerBuilder::create);
+            Text descriptionText = bcv.getDescription();
+            if (descriptionText != null) {
+                boolOptionBuilder.description(OptionDescription.of(descriptionText));
+            }
+            return boolOptionBuilder.build();
+        } else {
+            throw new UnsupportedOperationException("Unknown entry: " + configEntry.getClass().getName());
+        }
+    }
+}
