ba2d67157ef9215ab9202c891c5f4a7b3fe4971d
Duppy_Conqueror <duppyconqueror206@gmail.com>
2026-08-28 17:53:57 +0800

    fix: Smart redstone wire connection configurable not working due to Neoforge abstraction

---
+++ src/main/java/net/duppy_conqueror/logic_gate/mixin/IBlockExtensionMixin.java
@@ -0,0 +1,32 @@
+package net.duppy_conqueror.logic_gate.mixin;
+
+import net.duppy_conqueror.logic_gate.block.LogicGateBlock;
+import net.duppy_conqueror.logic_gate.block.ModBlocks;
+import net.duppy_conqueror.logic_gate.block.enums.LogicGateMode;
+import net.duppy_conqueror.logic_gate.config.ModConfig;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.common.extensions.IBlockExtension;
+import org.jspecify.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(IBlockExtension.class)
+public interface IBlockExtensionMixin {
+    @Inject(at = @At("HEAD"), method = "canConnectRedstone", cancellable = true)
+    private static void onCanConnectRedstone_handleSmartConnection(BlockState state, BlockGetter level, BlockPos pos, @Nullable Direction direction, CallbackInfoReturnable<Boolean> cir) {
+        if (ModConfig.SMART_REDSTONE_WIRE_CONNECTION.get() && state.is(ModBlocks.LOGIC_GATE) && direction != null) {
+            LogicGateMode mode = state.getValue(LogicGateBlock.MODE);
+            Direction facing = state.getValue(LogicGateBlock.FACING);
+            if (mode.isSingleInput()) {
+                cir.setReturnValue(facing.getAxis() == direction.getAxis());
+            } else {
+                cir.setReturnValue(!facing.equals(direction.getOpposite()));
+            }
+        }
+    }
+}
